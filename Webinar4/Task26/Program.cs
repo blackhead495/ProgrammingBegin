be2d512788e6e9199a4378a34e4b85c210392d7d
Dzhalilov <df@dealmed.ru>
2022-03-24 09:43:30 +0300

    Add complete Project Task26

---
+++ Webinar4/Task26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, которая принимает число и выдает количество цифр в этом числе
+
+using System;
+
+namespace Task26
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите любое целое число: ");
+            int number = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine($"{number} -> {NumDigits(number)}");
+
+            int NumDigits(int A)
+            {
+                int i = 0;
+                while (A != 0)
+                {
+                    A /= 10;
+                    i++;
+                }
+
+                return i;
+            }
+        }
+    }
+}
