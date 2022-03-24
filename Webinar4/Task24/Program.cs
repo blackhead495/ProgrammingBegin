fd5484be3a24088ea7353cde93c2b2cf56104d9e
Dzhalilov <df@dealmed.ru>
2022-03-24 08:13:31 +0300

    Add Complete file Task24

---
+++ Webinar4/Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, которая принимает на вход число А и выдает суммму чисел от 1 до А
+
+using System;
+
+namespace Task24
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите любое целое число: ");
+            int number = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine($"{number} -> {SummN(number)}");
+
+            int SummN(int A)
+            {
+                int summ = 0;
+
+                for (int i = 1; i <= A; i++)
+                {
+                    summ += i;
+                }
+
+                return summ;
+            }
+        }
+    }
+}
