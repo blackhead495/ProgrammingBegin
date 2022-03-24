6950acbf1b431d69b4d72931d1b812ee8bda9807
Dzhalilov <df@dealmed.ru>
2022-03-24 16:39:12 +0300

    Add complete file Task28

---
+++ Webinar4/Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task28
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите любое целое число: ");
+            int number = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine($"{number} -> {Factorial(number)}");
+
+            int Factorial(int A)
+            {
+                int Mult = 1;
+                for (int i = 1; i <= A; i++)
+                {
+                    Mult *= i;
+                }
+
+                return Mult;
+            }
+        }
+    }
+}
